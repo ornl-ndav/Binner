2a5a43919fbc4f395db555aba4dca6bc39b0be1a
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2008-10-13 15:30:27 +0000

    new buildall script

--- trunk/binner/script/buildall.sh
+++ trunk/binner/script/buildall.sh
@@ -4,6 +4,7 @@
 #build
 cd ../vcb/src
 make
+make -f makefile_withgl
 make clean
 
 cd ../libtestsrc
@@ -14,6 +15,8 @@ make clean
 cd ../../src
 make all
 make install
+make -f makefile_withgl seeall
+mv seeall ../bin
 make clean
 
 exit 0
