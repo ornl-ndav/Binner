3291681377885a5508d0bd3085f1e00a543f2e22
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2009-01-14 21:53:26 +0000

    new code added to export vtk volume from compressed rebinning results.

--- trunk/binner/src/binnerio.c
+++ trunk/binner/src/binnerio.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <assert.h>
 #include "binnerio.h"
+#include "vcbutils.h"
 
 void printvertexf(float *v, int ndim)
 {
@@ -176,5 +177,130 @@ void binnerout_phcd(int nfacets, int * nverts, double *v)
 		}
 		printf("\n");
 	}
+}
+
+void output_with_compression(char * fname,
+							int * sz,
+							double * vol)
+{
+	int i, j, k, nvox;
+	double         * dvol;
+	unsigned int   * hash;
+	unsigned short * xyz;
+	int    ori[3], size[3];
+	char fullname[256];
+
+	nvox = 0;
+	for (i = 0; i < sz[0]; i ++)
+		for (j = 0; j <sz[1]; j ++)
+			for (k = 0; k <sz[2]; k ++) {
+
+				if (vol[(i * sz[1] + j)*sz[2] + k] > 1e-16)
+				    nvox ++;
+			}
+	
+	dvol = malloc(nvox * sizeof(double));
+	hash = malloc(sz[0]*sz[1]*sizeof(int));
+	xyz  = malloc(nvox * 3 * sizeof(unsigned short));
+
+	nvox = 0;
+	for (i = 0; i < sz[0]; i ++)
+		for (j = 0; j <sz[1]; j ++)
+		{
+			hash[i * sz[1] + j] = nvox;
+			for (k = 0; k <sz[2]; k ++) {
+
+				if (vol[(i * sz[1] + j)*sz[2] + k] > 1e-16)
+				{
+					dvol[nvox] = vol[(i * sz[1] + j)*sz[2] + k];
+					xyz[nvox*3+0] = (unsigned short)(i);
+					xyz[nvox*3+1] = (unsigned short)(j);
+					xyz[nvox*3+2] = (unsigned short)(k);
+				    nvox ++;
+				}
+			}
+		}
+
+	ori[0] = ori[1] = ori[2] = 0;
+	size[0] = nvox;
+	sprintf(fullname, "%s.bin", fname);
+	vcbGenBinm(fullname, VCB_DOUBLE, 1, ori, size, 1, dvol);
+	/*
+	sprintf(fullname, "%s.bin", fname);
+	vcbGenBinm(fullname, VCB_UNSIGNEDINT, 2, ori, sz, 1, hash);
+	*/
+	sprintf(fullname, "%s.bin", fname);
+	vcbGenBinm(fullname, VCB_UNSIGNEDSHORT, 1, ori, size, 3, xyz);
+
+	free(dvol);
+	free(hash);
+	free(xyz);
+}
+
+void export_VTKvol(char * fname, double * orig, int * sz, double * cellsize, double * vol)
+{
+	FILE * fp;
+	char fullname[256];
+	int i, j, k;
+	double val;
+	float * cvol;
+	
+	sprintf(fullname, "%s.vtk", fname);
+	fp = fopen(fullname, "w");
+	
+	fprintf(fp, "# vtk DataFile Version 1.0\n");
+	fprintf(fp, "rebinned Qxyz histogram %s\n", fname);
+	fprintf(fp, "BINARY\n");
+	fprintf(fp, "DATASET STRUCTURED_POINTS\n");
+	fprintf(fp, "DIMENSIONS %d %d %d\n", sz[0], sz[1], sz[2]);
+	fprintf(fp, "ASPECT_RATIO 1.0 1.0 1.0\n");
+	fprintf(fp, "ORIGIN %e %e %e\n",orig[0]*cellsize[0], 
+									orig[1]*cellsize[1], 
+									orig[2]*cellsize[2]);
+	fprintf(fp, "SPACING %e %e %e\n", cellsize[0], cellsize[1], cellsize[2]);	
+	fprintf(fp, "POINT_DATA %d\n", sz[0]*sz[1]*sz[2]);
+	fprintf(fp, "SCALARS scalars float\n");
+	fprintf(fp, "LOOKUP_TABLE default\n");
+	
+	cvol = malloc(sizeof(float)*sz[0]*sz[1]*sz[2]);
+	for (i = 0; i < sz[0]; i ++)
+		for (j = 0; j <sz[1]; j ++)
+			for (k = 0; k <sz[2]; k ++) {
+/*
+				val = log10(vol[(i * sz[1] + j) * sz[2] +k]);
+				val += 16;
+				val *= 16;
+				if (val < 0) val = 0;
+*/
+				val = vol[(i * sz[1] + j) * sz[2] +k];
+				cvol[(k * sz[1] + j) * sz[0] +i] = (float)val;
+			}
+
+	for (i = 0; i < sz[0]*sz[1]*sz[2]; i ++)
+		vcbToggleEndian(cvol+i, sizeof(float));
+
+	fwrite(cvol, sizeof(float), sz[0]*sz[1]*sz[2], fp);
+	free(cvol); 
+	fclose(fp);
+}
+
+void export_VTKhdr(char * fname, int * orig, int * sz, double * cellsize)
+{
+	FILE * fp;
+	char fullname[256];
+
+	sprintf(fullname, "%s/run.hdr", fname);
+	fp = fopen(fullname, "w");
+	
+	fprintf(fp, "REBINNED Qxyz HISTOGRAM\n%s\n", fname);
+	fprintf(fp, "DIMENSIONS %d %d %d\n", sz[0], sz[1], sz[2]);
+	fprintf(fp, "ORIGIN %e %e %e\n",orig[0]*cellsize[0], 
+									orig[1]*cellsize[1], 
+									orig[2]*cellsize[2]);
+	fprintf(fp, "SPACING %e %e %e\n", cellsize[0], cellsize[1], cellsize[2]);	
+	fprintf(fp, "Total NUMBER OF BINS %d\n", sz[0]*sz[1]*sz[2]);
+	fprintf(fp, "BINARY FLOAT VOLUME REQUIRES %d byes\n", sz[0]*sz[1]*sz[2]*sizeof(float));
+	
+	fclose(fp);
 }
 
