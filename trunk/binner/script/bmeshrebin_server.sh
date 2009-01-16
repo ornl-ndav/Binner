e9477e18957515a5851d359301dfd7730a82f808
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2009-01-16 07:16:45 +0000

    date/time line added in each logging

--- trunk/binner/script/bmeshrebin_server.sh
+++ trunk/binner/script/bmeshrebin_server.sh
@@ -6,6 +6,10 @@ TOP=..
 serveproc="${TOP}/bin/serve"
 rebinproc="${TOP}/bin/bmeshrebin"
 logfile="${TOP}/log/bmeshrebin_server.log"
+
+echo -n "server up at        : " >> $logfile
+echo `date` >> $logfile
+
 $serveproc $1 $2 2>>$logfile | $rebinproc $3 2>>$logfile
 
 echo >> $logfile
