acfa779340a0073c25b5ad83414ebb4a19a631ad
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2009-01-19 22:04:15 +0000

    a minor tweak

--- trunk/binner/script/generate_report.sh
+++ trunk/binner/script/generate_report.sh
@@ -13,7 +13,7 @@ grep percentage $logfile | cut -f2 -d":" | cut -f2 -d" " | cut -f1 -d"%" > t4
 echo "res     num_p/s sec     %non_empty"
 paste t1 t2 t3 t4
 
-#rm t1 t2 t3
+rm t1 t2 t3 t4
 
 exit 0
 
