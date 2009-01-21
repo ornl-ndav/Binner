89ebc5f05311ac36b7362ec097674be94c1edc83
jhm <huangj@cs.utk.edu>
2009-01-21 16:13:42 +0000

    a new testing code

---
+++ binner/src/counts.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include "atypes.h"
+#include "binnerio.h"
+
+/* application specific data starts here */
+int     nverts, npara;
+float   vdata[1024];
+
+/* end of application specific data */
+bbox abbox;
+
+char inputbuf[1024];
+
+int list_id;
+
+float xmin, xmax, ymin, ymax;
+
+int main(int argc, char ** argv)
+{
+  int   i, j, n, ndims, orig, nvals, dummy;
+  int   sliceid;
+  float hitcnt, hiterr, corners[8][4];
+  float totalhitcnt, totalhiterr;
+  double red;
+
+  totalhitcnt = 0;
+  totalhiterr = 0;
+  
+  for (npara = 0; (n = get_pixelf(&sliceid,&hitcnt,&hiterr,corners)) > 0; npara ++) {
+	totalhitcnt += hitcnt;
+	totalhiterr += hiterr;
+  }
+
+  printf("number of parallelipeds = %d\n",npara);
+  printf("totalhitcnt = %f, totalhiterr = %f\n", totalhitcnt, totalhiterr);
+  
+  return 0;
+}
