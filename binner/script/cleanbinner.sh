58631c0dc318af0d1de7e0854c712bc045190672
jhm <huangj@cs.utk.edu>
2009-01-22 15:08:24 +0000

    new updated scripting to better clean the binner directory

--- binner/script/cleanbinner.sh
+++ binner/script/cleanbinner.sh
@@ -1,9 +1,17 @@
 #!/bin/csh -f
 
+rm ../*~
+rm ../vcb/*~
 
 cd ../bin
-rm  compvol rebin seeall bslicer rot3d clip3d gencell scale3d tran3d collector getfrom giveto netrebin sorter pix2mesh seepara serve bmeshrebin seebmeshvol
+rm  compvol rebin seeall bslicer rot3d clip3d gencell scale3d tran3d collector getfrom giveto netrebin sorter pix2mesh seepara serve bmeshrebin seebmeshvol seebmeshvolc bounds counts export2vcb export2vtk largebmeshrebin
+rm *~
 rm *.d
+rm -rf MR*
+
+cd ../script
+rm *~
+rm -rf MR*
 
 cd ../src
 make clean
