61094f489919a432ba53f1714d450343cf08b332
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2009-05-15 19:15:34 +0000

    more targeted search for GL libraries

--- trunk/binner/vcb/CMakeModules/OpenGLConfig.cmake
+++ trunk/binner/vcb/CMakeModules/OpenGLConfig.cmake
@@ -7,16 +7,20 @@ find_path(OPENGL_INCLUDE_DIR gl.h
    /usr/include)
 
 find_library(OPENGL_gl_LIBRARY NAMES GL gl
-             PATHS /usr/local/lib64 
-				   /usr/lib64 
+             PATHS /usr/local/lib64 NO_DEFAULT_PATH)
+
+find_library(OPENGL_gl_LIBRARY NAMES GL gl
+             PATHS /usr/lib64 
 				   /usr/local/lib 
 				   /usr/lib 
 				   /usr/X11R6/lib) 
 
 find_library(OPENGL_glu_LIBRARY NAMES GLU glu
              PATHS ${OPENGL_gl_LIBRARY} 
-				   /usr/local/lib64 
-				   /usr/lib64 
+				   /usr/local/lib64 NO_DEFAULT_PATH)
+	
+find_library(OPENGL_glu_LIBRARY NAMES GLU glu
+             PATHS /usr/lib64 
 				   /usr/local/lib 
 				   /usr/lib
 				   /usr/X11R6/lib) 
