ad5e57f17273e9fd34dfb5c162bb6c7f81c8b297
jhm <huangj@cs.utk.edu>
2009-05-15 19:02:48 +0000

    new glut search

--- binner/vcb/CMakeModules/OpenGLConfig.cmake
+++ binner/vcb/CMakeModules/OpenGLConfig.cmake
@@ -2,10 +2,9 @@
 
 find_path(OPENGL_INCLUDE_DIR gl.h
    /usr/local/include/GL
-   /usr/local/include
    /usr/include/GL
-   /usr/include
-   )
+   /usr/local/include
+   /usr/include)
 
 find_library(OPENGL_gl_LIBRARY NAMES GL gl
              PATHS /usr/local/lib64 
@@ -24,10 +23,9 @@ find_library(OPENGL_glu_LIBRARY NAMES GLU glu
 
 find_path(GLUT_INCLUDE_DIR glut.h
    /usr/local/include/GL
-   /usr/local/include
    /usr/include/GL
-   /usr/include
-   )
+   /usr/local/include
+   /usr/include)
 
 find_library(GLUT_LIBRARY NAMES glut GLUT
              PATHS ${OPENGL_gl_LIBRARY} 
