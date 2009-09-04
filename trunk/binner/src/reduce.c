a8a64eece1a78c85970bed31b82de0ce2df22c63
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2009-09-04 18:34:05 +0000

    refactoring code to expose a reducefunc API for flexible specification of reduce function used in the map/reduce model. This is to improve the interface of multi-threaded execution of rebinner

--- trunk/binner/src/reduce.c
+++ trunk/binner/src/reduce.c
@@ -5,22 +5,27 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-#define BUFSIZE 64*1024*1024
+#include "reducefunc.h"
+
+#define BUFSIZE 4*1024*1024
 
 int main(int argc, char ** argv)
 {
 	fd_set readfds, masterfds;
 	int i, n, m, nbytes = 0, ninputs;
 	char * netbuf;
+	int unitsize, toread;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "usage: sink number_of_input_streams\n");
+		fprintf(stderr, "usage: reduce number_of_input_streams\n");
 		fprintf(stderr, "%s %s\n", argv[0], argv[1]);
 		exit(1);
 	}
 	
 	close(0);
+
+	unitsize = reducefunc_unitsize();
 
 	ninputs = (int)(atof(argv[1]));
 
@@ -41,30 +46,35 @@ int main(int argc, char ** argv)
 		
 		if ((n = select(ninputs + 3, &readfds, NULL, NULL, NULL)) < 0)
 		{
-			perror("select failed in sink");
+			perror("select failed in reduce");
 			exit(2);
 		}
 
 #if REBINDEBUG
-		fprintf(stderr, "sink select returned %d, ninputs = %d \n", n, ninputs);
+		fprintf(stderr, "reduce select returned %d, ninputs = %d \n", n, ninputs);
 #endif
 		
 		for (i = 3; i < 3 + ninputs; i ++)
 			if (FD_ISSET(i, &readfds))
 			{
-				n = read(i, netbuf, BUFSIZE);
+				for (n = 0, toread = BUFSIZE; toread > 0; )
+				{
+					n += read(i, netbuf+n, toread);
+					toread = n - (n / unitsize) * unitsize;
+				}
+
 #if REBINDEBUG
-				fprintf(stder, "sink read %d bytes from fd: %d\n", n, i);
+				fprintf(stder, "reduce read %d bytes from fd: %d\n", n, i);
 #endif
 				if (n > 0)
 				{
-					//write(1, netbuf, n);
+					reducefunc(netbuf, n/unitsize);
 					nbytes += n;
 				}
 				else
 				{
 #if REBINDEBUG
-					fprintf(stderr,"sink input %d is done\n", i - 3);
+					fprintf(stderr,"reduce input %d is done\n", i - 3);
 #endif
 					FD_CLR(i, &masterfds);
 					m --;
@@ -74,9 +84,9 @@ int main(int argc, char ** argv)
 	}
 
 #if REBINDEBUG
-	fprintf(stderr,"sink ninputs = %d \n", ninputs);
+	fprintf(stderr,"reduce ninputs = %d \n", ninputs);
 #endif
-	fprintf(stderr,"sink wrote %d bytes.\n", nbytes);
+	fprintf(stderr,"reduce wrote %d bytes.\n", nbytes);
 	free(netbuf);
 
 	return 0;
