e8630617dbd4ec2bd7293e7939978869207b4d9f
jhm <jhm@039da35c-9a4d-0410-a492-8217ab67a9c6>
2010-07-28 21:06:44 +0000

    separating common macros into the same header file

---
+++ trunk/binner/include/macros.h
@@ -0,0 +1,18 @@
+/**
+ * $Id$
+ *
+ * \file include/macros.h
+ */
+
+
+
+/*! 
+   \def BINNER_EPSILON
+   Rebinner truncates all numbers smaller than threshold to zero.
+*/
+#ifndef BINNER_EPSILON
+#define BINNER_EPSILON 1e-6
+#endif
+
+/*! A convenience macro to compute the norm of a vector \a n.*/
+#define BINNER_NORM(n)	sqrt((n)[0]*(n)[0]+(n)[1]*(n)[1]+(n)[2]*(n)[2])
